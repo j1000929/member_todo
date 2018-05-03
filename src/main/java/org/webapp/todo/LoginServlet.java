48a750f603404ee1c03abeefdb945e80c008645f
201-10 <201-10@201-10-PC>
2018-05-03 17:55:16 +0900

    login

--- src/main/java/org/webapp/todo/LoginServlet.java
+++ src/main/java/org/webapp/todo/LoginServlet.java
@@ -3,6 +3,7 @@ package org.webapp.todo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,20 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 
 //어떤 url에 반응하는 서블릿인지 설정
 
-@WebServlet(urlPatterns="/login.do")
+//@WebServlet(urlPatterns="/login.do")
 public class LoginServlet extends HttpServlet{//Http서블릿을 상속해야함
 
 @Override
 protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-		throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Yahoo</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("My First Servlet");
-		out.println("</body>");
-		out.println("</html>");
+		throws IOException, ServletException {
+		
+		String name = request.getParameter("name");
+		String password =request.getParameter("password");
+		request.setAttribute("name", name);
+		request.setAttribute("password", password);
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);//
 	}
 }
